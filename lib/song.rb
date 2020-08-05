7cb8f0aa1e4a6d772d619e35c8a9bd5c98c5da7f
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-08-05 03:54:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,29 +2,29 @@ class Song
     
     attr_accessor :name, :artist, :genre
   
-    @@song_count = 0
+    @@count = 0
     @@genres = []
     @@artists = []
   
-    def initialize(name, artist, genre)
-      @name = name
+    def initialize(song_name, artist, genre)
+      @name = song_name
       @artist = artist
       @genre = genre
       @@genres << @genre
       @@artists << @artist
-      @@song_count +=1
+      @@count +=1
     end
   
     def self.count
-      @@song_count
+      @@count
     end
 
     def self.genres
-        @@genres.uniq
+        @@genres.uniq!
       end
     
       def self.artists
-        @@artists.uniq
+        @@artists.uniq!
       end
     
       def self.genre_count
